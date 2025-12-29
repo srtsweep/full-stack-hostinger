4ad8ac396642e4f56349f0b4b61396c05b97b45c
Mayank gupta <nisebhayop.69@gmail.com>
2025-12-30 03:36:34 +0530

    add production url

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
+      "https://full-stack-hostinger-client.onrender.com/",
       // TODO : add production url
     ],
     credentials: true,
